eb89e469d085bd7ba7661d8ba2b34b1b08bea912
Namie367 <54255478+Namie367@users.noreply.github.com>
2019-08-19 10:39:39 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask import render_template
+import glob
+import os
+from datetime import datetime
+
+app = Flask("Namie")
+
+@app.route("/")
+def root():
+  ds = glob.glob("articles/*")
+  result = []
+  for d in ds:
+    fs = glob.glob(d + "/*.txt")
+    t = (d.split("/")[-1] , len(fs))
+    result.append(t)
+  return render_template("index.html" , d=result)
+
+
+@app.route("/category/<c>")
+def category(c):
+  fs = glob.glob("articles/" + c  + "/*.txt")
+  result = []
+  for i,d in enumerate(fs):
+    a = open(d)
+    article = a.read()
+    a.close()
+    dsplit = d.split("/")[-1].replace(".txt" , "")
+    m = os.path.getmtime(d)
+    z = str(datetime.utcfromtimestamp(m))
+    t = (i, dsplit , z, article)
+    result.append(t)
+  return render_template("category.html" , d = result)
+
+@app.route("/article/<e>")
+def article(e):
+  articles = glob.glob("articles/" + e  + "/*.txt")
+  result = []
+  for d in articles:
+    artsplit = d.split("/")[-1].replace(".txt" , "")
+    result.append(artsplit)
+  return render_template("article.html" , d = result)
+
+app.run(debug=True , port = '3000' , 
+host = '0.0.0.0')
+
